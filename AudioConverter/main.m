8e6fb9d42348f621fa58563755ceee6e13f5fedd
Xiao Quan 全笑 <58939349+all-laugh@users.noreply.github.com>
2021-12-21 12:09:19 -0500

    Initial Commit

---
+++ AudioConverter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AudioConverter
+//
+//  Created by Xiao Quan on 12/21/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
